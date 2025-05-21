014749cde0cd3a277685a0764de40417bdb7010b
shuki <shukiheftler@gmail.com>
2025-05-21 16:25:28 +0300

    tools

---
+++ ProjectIDF/Artillery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectIDF
+{
+    internal class Artillery : AttackTools
+    {
+        public override string BombType { get; set; }
+        public override int AmmunitionCapacity { get; set; }
+        public override string EffectiveAgainst { get; set; }
+        public Artillery(string bombType, int ammunitionCapacity, string effectiveAgainst)
+        {
+            BombType = bombType;
+            AmmunitionCapacity = ammunitionCapacity;
+            EffectiveAgainst = effectiveAgainst;
+        }
+    }
+}
---
+++ ProjectIDF/Zik.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectIDF
+{
+    internal class Zik : AttackTools
+    {
+
+        public override string BombType { get; set; }
+        public override int AmmunitionCapacity { get; set; }
+        public override string EffectiveAgainst { get; set; }
+        public Zik(string bombType, int ammunitionCapacity, string effectiveAgainst)
+        {
+            BombType = bombType;
+            AmmunitionCapacity = ammunitionCapacity;
+            EffectiveAgainst = effectiveAgainst;
+        }
+    }
+}
