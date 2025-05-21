921bb17907ebd725ee9bae31c0ed4bedf2521833
davidlandau770 <dal778770@gmail.com>
2025-05-21 14:09:28 +0300

    first commit

---
+++ ProjectIDF/IdfDefinition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectIDF
+{
+    internal class IdfDefinition : IOrganization
+    {
+        public string DateOfEstablishment {  get; set; }
+        public string CurrentCommander { get; set; }
+        public string[] CollectionOfStrike;
+
+        IdfDefinition(string dateOfEstablishment, string currentCommander)
+        {
+            DateOfEstablishment = dateOfEstablishment; // "may 26 1948";
+            CurrentCommander = currentCommander; // "eyal zamir";
+            CollectionOfStrike = new string[] { "aircraft", "drones", "artillery" };
+        }
+
+        //public string get()
+        //{
+        //    IdfDefinition d = new IdfDefinition("ds", "gghf");
+        //    return $"{d.get()}";
+        //}
+      
+    }
+}
---
+++ ProjectIDF/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectIDF
+{
+    internal class Program
+    {
+        static void MenuToUser()
+        {
+            while (true)
+            {
+                Console.WriteLine("Commander Console:\nChoose num: 1-10\n1. Intel Analysis\n2. Strike Availability\n3. Target Prioritization\n4. Strike Execution\n");
+                int.TryParse(Console.ReadLine(), out int num);
+                switch (num)
+                {
+                    case 1:
+                        //IntelAnalysis();
+                        break;
+                    case 2:
+                        //StrikeAvailability();
+                        break;
+                    case 3:
+                        //TargetPrioritization();
+                        break;
+                    case 4:
+                        //StrikeExecution();
+                        break;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            MenuToUser();
+        }
+    }
+}
