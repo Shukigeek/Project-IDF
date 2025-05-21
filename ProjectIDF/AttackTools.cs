0b5ce4b43885c8aecc0d6ba910fd313ef2518f92
shuki <shukiheftler@gmail.com>
2025-05-21 16:21:59 +0300

    terrorist

---
+++ ProjectIDF/AttackTools.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectIDF
+{
+    internal abstract class AttackTools
+    {
+        public abstract string BombType { get; set; }
+        public abstract int AmmunitionCapacity { get; set; }
+        public int FuelSupply { get; set; }
+        public abstract string EffectiveAgainst { get; set; }
+
+    }
+}
---
+++ ProjectIDF/Terrorist.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectIDF
+{
+    internal class Terrorist
+    {
+        string Name {  get; set; }
+        int Rank;
+        string Status;
+        string[] Weapon;
+        public int rank
+        {
+            get {  return Rank; }
+            set
+            {
+                if (value < 1)
+                {
+                    Rank = 1;
+                }else if (value > 5)
+                {
+                    Rank = 5;
+                }
+                else
+                {
+                    Rank = value;
+                }
+            }
+        }
+
+        public Terrorist(string name, int rank, string status, string[] weapon)
+        {
+            Name = name;
+            Rank = rank;
+            Status = status;
+            Weapon = weapon;
+        }
+    }
+}
